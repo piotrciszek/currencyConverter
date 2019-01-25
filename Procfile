7319449cf395de746a6d551ba9d0891c6b5e54a0
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2019-01-25 22:06:13 +0100

    dodanie convertera3

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT -Dspring.profiles.active=development -jar target/*.jar
---
+++ src/main/java/com/sda/exampleclient/ExampleclientApplication.java
@@ -0,0 +1,12 @@
+package com.sda.exampleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleclientApplication.class, args);
+	}
+}
